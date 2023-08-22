a4471422cd088eaedfa70fb5aef5a30bf40b7bbb
David <montanezdavid@gmail.com>
2023-08-22 11:42:16 -0500

    Configured create post functionality with api request URL

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -14,7 +14,28 @@ const CreatePrompt = () => {
     });
 
     const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
 
+        try {
+            const response = await fetch("/api/prompt/new",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    prompt: post.prompt,
+                    userId: session?.user.id,
+                    tag: post.tag
+                })
+            });
+
+            if (response.ok) {
+                Router.push("/");
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setSubmitting(false);
+        }
     };
 
     return (
