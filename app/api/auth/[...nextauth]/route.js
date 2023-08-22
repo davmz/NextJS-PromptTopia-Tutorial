3d13010f5a9c38813aa23cbdca86c26d8edcb4c9
David <montanezdavid@gmail.com>
2023-08-22 01:01:12 -0500

    Fixed issue where the mongodb was not being called and added all of the async functions under the callback object

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -13,39 +13,41 @@ const handler = NextAuth({
         })
     ],
 
-    async session({ session }) {
-        const sessionUser = await User.findOne({
-            email: session.user.email
-        });
-
-        session.user.id = sessionUser._id.toString();
-        return session; 
-    },
-
-    async signIn({ profile }) {
-        try {
-            await connectToDB();
-
-            // check if a user already exists
-            const userExists = await User.findOne({
-                email: profile.email
+    callbacks: {
+        async session({ session }) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
             });
-
-            // if not, create a new user
-            if (!userExists) {
-                await User.create({
-                    email: profile.email,
-                    username: profile.name.replace(" ", "").toLowerCase(),
-                    image: profile.picture
+    
+            session.user.id = sessionUser._id.toString();
+            return session; 
+        },
+    
+        async signIn({ profile }) {
+            try {
+                await connectToDB();
+    
+                // check if a user already exists
+                const userExists = await User.findOne({
+                    email: profile.email
                 });
+    
+                // if not, create a new user
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture
+                    });
+                }
+    
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false;
             }
-
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    }
+        }    
+    },
 });
 
 export { handler as GET, handler as POST};
