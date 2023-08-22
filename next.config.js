00a417e1dafec0aa8a12890cf85103fb9234e645
David <montanezdavid@gmail.com>
2023-08-22 00:50:37 -0500

    Configured MongoDB connections and created User Schema as well as ENV secrets

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
 
-module.exports = nextConfig
+    images: {
+        domains: ["lh3.googleusercontent.com"],
+    },
+
+    webpack(config) {
+        config.experiments = {
+            ...config.experiments,
+            topLevelAwait: true,
+        };
+        return config;
+    },
+  };
+  
+  module.exports = nextConfig;
