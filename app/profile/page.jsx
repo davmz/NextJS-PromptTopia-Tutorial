33eb2dd437d51bc58f72c4b7299fb2ab1bc0c79c
David <montanezdavid@gmail.com>
2023-09-02 01:00:20 -0500

    Removed commented code

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,24 +28,6 @@ const MyProfile = () => {
         router.push(`/update-prompt?id=${post._id}`);
     };
 
-    // const handleDelete = async () => {
-    //     const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
-
-    //     if (hasConfirmed) {
-    //         try {
-    //             await fetch(`/api/prompt/${post._id.toString()}`, {
-    //                 method: "DELETE",
-    //             });
-
-    //             const filteredPosts = posts.filter((p) => p._id !== post._id);
-
-    //             setPosts(filteredPosts);
-    //         } catch (error) {
-    //             console.log(error);
-    //         }
-    //     }
-    // };
-
     const handleDelete = async (post) => {
       const hasConfirmed = confirm("Are you sure you want to delete this post?");
   
