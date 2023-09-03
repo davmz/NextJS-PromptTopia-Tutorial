6345e43d67b6f85eb0c598c6c832f4728d3d3c97
David <montanezdavid@gmail.com>
2023-09-03 01:21:58 -0500

    Updated title metadata

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav";
 import Provider from "@components/Provider";
 
 export const metadata = {
-    title: "Promptopia",
+    title: "Promptopia - David Montanez",
     description: "Discover & Share AI Prompts"
 }
 
